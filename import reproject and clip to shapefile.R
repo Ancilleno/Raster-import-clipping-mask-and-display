0ad770eb70c9c9b3f94c1f2d8f3bcb81424df159
Ancilleno Orlando Davis, M.Sc <ancillenodavis@gmail.com>
2018-06-15 13:38:05 -0400

    update the import and summary of the raster image in R

--- import reproject and clip to shapefile.R
+++ import reproject and clip to shapefile.R
@@ -1,15 +1,15 @@
-#Ancilleno Davis
+#Importing Geotiffs and barplots of pixels and land area
 
-#This r-code has been developed by Ancilleno Davis in partial completion of the 
+#This r-code has been devloped by Ancilleno Davis in partial completion of the 
 #requirements for PhD in Ecology Evolution and Environmental Science at Miami University in Oxford, Ohio.
 
-#The purpose of this Rcode is to import habitat classification data 
-#generated in Google Earth Engine from Landsat 8 OLI reflectance data,
-#trim that classification data to the contiguous terrestrial area of Grand Bahama Island
-#display the resulting raster images
-#calculate the kappa coefficient for the confusion matrix and 
-#calculate percent cover and total area in each habitat 
-#This will allow more efficient summary and analysis of the remaining habitat data
+#The purpose of this Rcode is to#####
+###Import confusion matrix generate in Google Earth Engine Code API and calculate Cohen's Kappa
+#import habitat classification data
+#generated in Google Earth Engine from LAndsat 8 OLI reflectance data,
+#display that data as a raster image and calculate the area within each habitat class
+#clip that habitat classification data to the extent of a shapefile
+
 
 #### Initial workspace parameters####
 #set seed
@@ -18,12 +18,13 @@ set.seed(1981)
 #Set my working directory and load necessary libraries
 setwd("C:/Users/davisao2/Desktop/open source GIS/eBird r code")
 #note this working directory should include:
-#1: the geotiff of the raster data created using the Google Earth Engine habitat classification
+#1: the geotiff created using the Google Earth Engine habitat classification
+#2: The shapefile of the island outline
 
-#2: the shapefile of the outline of the terrestrial area of Grand Bahama and all associated files
+#code used to create the geoTIFF can be found at: https://code.earthengine.google.com/aaffec23a013f18ae898fe3eb9e4e1aa
+#necessary files can be found at:https://github.com/Ancilleno/Raster-import-clipping-mask-and-display
 
-
-#Load libraries
+#importing raster data
 require(caret)#this has confusionMatrix for determining raster accuracy
 require(cooccur)
 require(data.table)#to allow rearrangment of columns by their headings
@@ -39,16 +40,43 @@ require(lme4)
 require(lmerTest)
 require(plyr)
 require(psych)
-require(raster)#work with raster items
+require(raster)
 require(readr)
 require(readxl)
 require(reshape2)#contains dcast for reorganizing data into presence absence etc.
 require(rgdal)#read in shapefiles
 require(rgeos)
 require(RStoolbox)
-require(sp) #work with spatial items
+require(sp)
 
 dev.off()#this resets the graphics window so that margins and other adjustments are returned to default
+
+
+
+#determine habitat model accuracy using the confusion matrix#### 
+#generated from the Google Earth Engine Script.
+#Import the confusion matrix that was calculated by Google Earth Engine 
+###your confusionmatrix should be in a csv with no header rows##
+#using validation data
+confmatrix20177classes <-read_csv("7x7 class confusion matrix 2017.csv", 
+                                  col_names = TRUE)
+##View(confmatrix20177classes)
+confmatrix20177classes<-as.data.frame(confmatrix20177classes)
+rownames(confmatrix20177classes)<-
+  colnames(confmatrix20177classes)<-
+  c("Water",
+    "Pine",
+    "Wetland",
+    "Sand",
+    "Urban",
+    "Grass",
+    "HWTC")
+
+#calculate the cohen's kappa coefficient to determine the agreement between 
+#the classification and validation data
+Kappa.test(confmatrix20177classes,y=NULL, conf.level=0.95)
+
+
 
 #Assign the colors I would like to represent my habitat classes####
 
@@ -69,6 +97,22 @@ legend=c("Water",
          "Urban", 
          "Grass", 
          "HWTC")
+
+###Import Rasters of habitat classification ####
+
+#Random Forests classification map created using
+#6 Landsat 8 OLI bands 2,3,4,5,6,7
+#collected during 2017-01-01 through 2017-12-31
+
+RF7Classes2017<-raster("RFclass6bands30m7classes2017.tif")
+RF7Classes2017@crs #finds the coordinate reference system which is already WGS84
+RF7Classes2017@extent #gives me the boundaries of the raster
+
+plot(RF7Classes2017, 
+     las=1, #rotates the y-axis labels horizontal
+     ylab="Latitude",
+     xlab="Longitude",
+     main = "Grand Bahama Habitat Map")
 #import the Outline of Grand Bahama Island and reproject to WGS84####
 GBOutline<-readOGR(".","Grand_Bahama_Outline")
 print(proj4string(GBOutline)) #details of the projection for GBOutline
@@ -87,30 +131,15 @@ plot(GBOutlineWGS84,
      las=1,
      #ylab="Latitude",
      # xlab="Longitude",
-     main="Grand Bahama Contiguous Area")
-###Import Rasters of habitat classification ####
-
-#Random Forests classification map created using
-#6 Landsat 8 OLI bands 2,3,4,5,6,7
-#collected during 2017-01-01 through 2017-12-31
-
-RF7Classes2017<-raster("RFclass6bands30m7classes2017.tif")
-RF7Classes2017@crs #finds the coordinate reference system which is already WGS84
-RF7Classes2017@extent #gives me the boundaries of the raster
-
-plot(RF7Classes2017, 
-     las=1, #rotates the y-axis labels horizontal
-     ylab="Latitude",
-     xlab="Longitude",
-     main = "Grand Bahama Habitat Map")
+     main="Grand Bahama Contiguous Area WGS84")
 #overlay the reprojected shapefile on the raster####
 plot(GBOutlineWGS84, add=T)
 #contiguous area habitat
 
-#Clip area to the contiguous area of Grand Bahama Island####
+#Clip the raster area to the contiguous area of Grand Bahama Island####
 GBcontiguoushab<-#this vector will hold our clipped raster
   mask(RF7Classes2017#This is the raster image to be clipped
-                      , GBOutlineWGS84)#This is the shape you are clipping to
+       , GBOutlineWGS84)#This is the shape you are clipping to
 
 plot(GBcontiguoushab); plot(GBOutlineWGS84, add=T) #Plot the trimmed raster and the outline
 summary(GBcontiguoushab) #note all the clipped area is now NA
@@ -120,18 +149,18 @@ writeRaster(GBcontiguoushab,#our new rater image
             , format="GTiff") #the raster format we are using.
 ##display the clipped raster####
 GBcontiguoushab.range<-cellStats(GBcontiguoushab, range)
-dev.off()
+
 plot(GBcontiguoushab,#plot this raster file
      col=col7class, #a list of colors make sure they are in the order of the classes you are plotting
      axis.args=list(
-      at= #how do you want the axis labels spread out?
-       seq(#sequential separations from the 
-        GBcontiguoushab.range[1],#lowest end of the range
-        GBcontiguoushab.range[2], #to the highest end of the range
-      1),#with one unit of separation between each tick mark
-      labels=legend, #at each tick mark, what do you want the label to say?
-     cex.axis=1),
-      axes=T ,
+       at= #how do you want the axis labels spread out?
+         seq(#sequential separations from the 
+           GBcontiguoushab.range[1],#lowest end of the range
+           GBcontiguoushab.range[2], #to the highest end of the range
+           1),#with one unit of separation between each tick mark
+       labels=legend, #at each tick mark, what do you want the label to say?
+       cex.axis=1),
+     axes=T ,
      main = "Grand Bahama Island, Bahamas habitat map", #title of the plot 
      ylab="Latitude", #left side 
      xlab="Longitude", #x axis label
@@ -139,13 +168,14 @@ plot(GBcontiguoushab,#plot this raster file
 )
 plot(GBOutlineWGS84, add=T)
 legend('bottomright', legend , fill=col7class, border="black",
-        col=col7class, bty='n', cex=1.5)
+       col=col7class, bty='n', cex=1.5)
 #summarize the number of pixels and the geographic area in each class####
 GBhabitatdf<-as.data.frame(GBcontiguoushab)
-habitatpixels<-count(GBhabitatdf, "RFclass6bands30m7classes2017")
+pixels<-count(GBhabitatdf, "RFclass6bands30m7classes2017")
+habitatpixels<-pixels$freq[1:7]
 habitatareakm2<- #a vector of the total area in each habitat type
-  (habitatpixels$freq[1:7] #1:7 selects the columns with the classes 0-6 and ignores the NA values
-                 )*0.0009 #0.0009 is the number of km2 in a 30 x 30 m pixel (Landsat 8 imagery)
+  (pixels$freq[1:7] #1:7 selects the columns with the classes 0-6 and ignores the NA values
+  )*0.0009 #0.0009 is the number of km2 in a 30 x 30 m pixel (Landsat 8 imagery)
 habitatareakm2
 totalpixels<- #vector for the total pixels in the study area
   sum(habitatpixels$freq[1:7])
@@ -156,44 +186,44 @@ percentarea<-habitatareakm2/totalarea #%of total area in each habitat type
 habitat<-data.frame(legend,habitatpixels$freq[1:7],habitatareakm2,percentarea)
 
 ##Display barplots of the pixel area in each habitat type.####
-BpGBHabitatPixels<-barplot(GBcontiguoushab, 
-                     col=col7class,
-                     axes=F,
-                     main="Pixels per terrestrial habitat class on Grand Bahama 2017", 
-                     xlab="Habitat type",
-                     ylab="Number of Pixels",
-                     horiz=FALSE,
-                     ylim=c(0,400000),
-                     names.arg=c("Water", 
-                                 "Pine", 
-                                 "Wetland", 
-                                 "Sand", 
-                                 "Urban", 
-                                 "Grass", 
-                                 "HWTC"),
-                     las=1)
+BpGBHabitatPixels<-barplot(habitatpixels, 
+                           col=col7class,
+                           axes=F,
+                           main="Pixels per terrestrial habitat class on Grand Bahama, 2017", 
+                           xlab="Habitat type",
+                           ylab="Number of Pixels",
+                           horiz=FALSE,
+                           ylim=c(0,400000),
+                           names.arg=c("Water", 
+                                       "Pine", 
+                                       "Wetland", 
+                                       "Sand", 
+                                       "Urban", 
+                                       "Grass", 
+                                       "HWTC"),
+                           las=1)
 text(BpGBHabitatPixels, 
-     habitatpixels$freq[1:7], 
-     label=habitatpixels$freq[1:7], 
+     habitatpixels, 
+     label=habitatpixels, 
      pos = 3, 
      xpd = NA)
 ##Display barplots of the km2 area in each habitat type.####
 
 BpGBHabitatkm2<-barplot(habitatareakm2, 
-                     col=col7class,
-                     axes=F,
-                     main="Square Km per terrestrial habitat class on Grand Bahama 2017", 
-                     xlab="Habitat type",
-                     ylab="Area (sq.Km)",
-                     horiz=FALSE,
-                     ylim=c(0,400),
-                     names.arg=c("Water", 
-                                 "Pine", 
-                                 "Wetland", 
-                                 "Sand", 
-                                 "Urban", 
-                                 "Grass", 
-                                 "HWTC"))
+                        col=col7class,
+                        axes=F,
+                        main="Square Km per terrestrial habitat class on Grand Bahama, 2017", 
+                        xlab="Habitat type",
+                        ylab="Area (sq.Km)",
+                        horiz=FALSE,
+                        ylim=c(0,400),
+                        names.arg=c("Water", 
+                                    "Pine", 
+                                    "Wetland", 
+                                    "Sand", 
+                                    "Urban", 
+                                    "Grass", 
+                                    "HWTC"))
 text(BpGBHabitatkm2, 
      habitatareakm2, 
      label=habitatareakm2, 
@@ -201,25 +231,4 @@ text(BpGBHabitatkm2,
      xpd = NA)
 
 
-###Import confusion matrix generated in Google Earth Engine Code API and calculate Cohen's Kappa####
-#Import the confusion matrix that was calculated by Google Earth Engine 
-#your confusionmatrix should be in a csv with no header rows
-#using validation data
-confmatrix20177classes <-read_csv("7x7 class confusion matrix 2017.csv", 
-                                  col_names = TRUE)
-##View(confmatrix20177classes)
-confmatrix20177classes<-as.data.frame(confmatrix20177classes)
-rownames(confmatrix20177classes)<-
-  colnames(confmatrix20177classes)<-
-  c("Water",
-    "Pine",
-    "Wetland",
-    "Sand",
-    "Urban",
-    "Grass",
-    "HWTC")
-
-#calculate the cohen's kappa coefficient to determine the agreement between 
-#the classification and validation data
-Kappa.test(confmatrix20177classes,y=NULL, conf.level=0.95)
 
